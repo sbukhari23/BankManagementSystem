88d60c4b988e806c436a594fc18e1a1f3c29dc6c
smrbbsmab <syedmuneebb1@gmail.com>
2024-12-28 18:04:37 +0500

    Modified DBConnection.java

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/BankDataBase";
     private static final String USER = "root";
-    private static final String PASSWORD = "MySQL@ROOT123#";
+    private static final String PASSWORD = "hassan.open";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
